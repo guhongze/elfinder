639e4da1d75599b8a93035849b535187f21847d4
Jimmy <zhengge2012@gmail.com>
2017-11-28 15:31:04 +0800

    add setup file and tests

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import elfinder
+
+# Try to detect if we have Pillow installed.
+imaging_library = list()
+try:
+    import Image # PIL does this, Pillow does not.
+except ImportError:
+    # Check to see if Pillow is installed...
+    try:
+        from PIL import Image
+    except ImportError:
+        # Prefer Pillow to PIL
+        imaging_library.append('Pillow>=2.0.0')
+
+setup(
+      name='elfinder',
+      url='https://github.com/jimmy201602/elfinder',
+      version = elfinder.__version__,
+      description='Elfinder-based file management solution for Django',
+      long_description=open('README.rst', 'rt').read(),
+      author='Jimmy',
+      author_email='zhengge2012@gmail.com',
+      packages=find_packages(),
+      license='BSD',
+      classifiers = [
+        'Development Status :: 0.01',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+        ],
+      include_package_data = True,
+      install_requires = [
+        "Django>=1.5",
+        "python-magic==0.4.3"
+        ] + imaging_library,
+)
